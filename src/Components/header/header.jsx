3722c961ba0372aa50ccb65f54920c8b74c22ba8
ElFonsi <a.magallanes@alumno.etec.um.edu.ar>
2024-11-08 12:03:46 -0300

    Sprint 8 commi 15

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -79,18 +79,4 @@ const Header = () => {
     );
 };
 
-// const top100films = [
-//     { title: 'The Shawshank Redemption', year: 1994 },
-//     { title: 'The Godfather', year: 1972 },
-//     { title: 'The Godfather: Part II', year: 1974 },
-//     { title: 'The Dark Knight', year: 2008 },
-//     { title: '12 Angry Men', year: 1957 },
-//     { title: "Schindler's List", year: 1993 },
-//     { title: 'Pulp Fiction', year: 1994 },
-//     {
-//       title: 'The Lord of the Rings: The Return of the King',
-//       year: 2003,
-//     }
-// ];
-
 export default Header;
